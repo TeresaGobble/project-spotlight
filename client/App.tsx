c2d7e8314b6536d399b5f7ffedda85b66a5e2364
Charlie Wilshire <charliewilshire@Charlies-MacBook-Pro.local>
2022-03-31 16:24:33 -0400

    initial component shape for SearchTable.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,4 +1,16 @@
-// import React from 'react';
+import React from 'react';
+import SearchTable from './SearchTable';
+
+const App = () => {
+  return (
+    <div id='app-root'>
+      <h1>This is the start of the App</h1>
+      <SearchTable />
+    </div>
+  )
+}
+
+export default App;
 
 // include firebase in typescript here, or as its own component
 
