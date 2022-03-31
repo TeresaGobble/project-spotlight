50972e6f3d9050b3463a8210784f248155f794e5
Charlie Wilshire <charliewilshire@Charlies-MacBook-Pro.local>
2022-03-31 18:06:03 -0400

    client folder not being invoked onto DOM, server 's index.tx is working successfully with npm start

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, request, Request, Response } from 'express';
 // import {Axios} from 'axios';
-const port = 3000;
+const port = 3210;
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(express.static("client"));
 
 app.get("/", (Request, Response) => {
   console.log("meow");
