ca602aeb5446754fb2b7855c4b4f4299b3d49509
Anisah Majeed <anisahmajeed4@gmail.com>
2022-04-06 19:08:53 -0400

    before merges

--- client/App.tsx
+++ client/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import Dropdowns from './dropdowns';
+import Dropdowns from './Dropdowns';
+
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const App = () => {
 
   return (
-    <div>
-      <h2>This is from the App component!</h2>
-      <Dropdowns/>
+    // <div>
+    //   <h2>This is from the App component!</h2>
+    //   <MapContainer center={[51.505, -0.09]} zoom={13}>
+    //     <TileLayer
+    //       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     />
+    //     <Marker position={[51.505, -0.09]}>
+    //       <Popup>
+    //         A pretty CSS3 popup. <br /> Easily customizable.
+    //       </Popup>
+    //     </Marker>
+    //   </MapContainer>
+    // </div>
+    <div className="map">
+      <h1>this is from the appp component</h1>
+      <Dropdowns />
     </div>
   )
 }
--- client/dropdowns.tsx
+++ client/dropdowns.tsx
@@ -3,7 +3,14 @@ import './index.css'
 
 const Dropdowns = () => (
 
-  <div>DROPDOWNS</div>
+  <div>
+    <select>
+      <option value="grapefruit">Grapefruit</option>
+      <option value="lime">Lime</option>
+      <option selected value="coconut">Coconut</option>
+      <option value="mango">Mango</option>
+    </select>
+  </div>
 
 )
 
