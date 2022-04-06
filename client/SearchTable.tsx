f6f3b9ffdfe78d4f93e39cb9b1766ba99d505b69
Charlie Wilshire <charliewilshire@Charlies-MBP.attlocal.net>
2022-04-06 19:07:40 -0400

    commit prior to 4/6 PR

--- client/SearchTable.tsx
+++ client/SearchTable.tsx
@@ -3,6 +3,350 @@ import { Chart } from "chart.js";
 const SearchTable = () => {
 
   // dummy search data for 3/20 - 3/21
+  // example set of data with only 10 results for viewing convenience, will be replaced with actual search results
+  const shortenedSearchResults = [
+        {
+            "id": "12647725",
+            "case_number": "JF180326",
+            "date": "2022-03-20T12:06:00.000",
+            "block": "032XX W CERMAK RD",
+            "iucr": "143A",
+            "primary_type": "WEAPONS VIOLATION",
+            "description": "UNLAWFUL POSSESSION - HANDGUN",
+            "location_description": "GAS STATION",
+            "arrest": true,
+            "domestic": false,
+            "beat": "1022",
+            "district": "010",
+            "ward": "24",
+            "community_area": "30",
+            "fbi_code": "15",
+            "x_coordinate": "1155271",
+            "y_coordinate": "1889197",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.851761264",
+            "longitude": "-87.705602716",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.705602716,
+                    41.851761264
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12648228",
+            "case_number": "JF180544",
+            "date": "2022-03-20T12:13:00.000",
+            "block": "004XX N WABASH AVE",
+            "iucr": "0610",
+            "primary_type": "BURGLARY",
+            "description": "FORCIBLE ENTRY",
+            "location_description": "SMALL RETAIL STORE",
+            "arrest": false,
+            "domestic": false,
+            "beat": "1834",
+            "district": "018",
+            "ward": "42",
+            "community_area": "8",
+            "fbi_code": "05",
+            "x_coordinate": "1176668",
+            "y_coordinate": "1903146",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.889582108",
+            "longitude": "-87.626649232",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.626649232,
+                    41.889582108
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12648916",
+            "case_number": "JF181170",
+            "date": "2022-03-20T12:23:00.000",
+            "block": "0000X N STATE ST",
+            "iucr": "0860",
+            "primary_type": "THEFT",
+            "description": "RETAIL THEFT",
+            "location_description": "DEPARTMENT STORE",
+            "arrest": false,
+            "domestic": false,
+            "beat": "0112",
+            "district": "001",
+            "ward": "42",
+            "community_area": "32",
+            "fbi_code": "06",
+            "x_coordinate": "1176362",
+            "y_coordinate": "1900577",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.882539542",
+            "longitude": "-87.627850543",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.627850543,
+                    41.882539542
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12647730",
+            "case_number": "JF180335",
+            "date": "2022-03-20T12:23:00.000",
+            "block": "005XX E 74TH ST",
+            "iucr": "0486",
+            "primary_type": "BATTERY",
+            "description": "DOMESTIC BATTERY SIMPLE",
+            "location_description": "APARTMENT",
+            "arrest": false,
+            "domestic": true,
+            "beat": "0323",
+            "district": "003",
+            "ward": "6",
+            "community_area": "69",
+            "fbi_code": "08B",
+            "x_coordinate": "1180951",
+            "y_coordinate": "1856063",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.760284994",
+            "longitude": "-87.61237142",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.61237142,
+                    41.760284994
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12649542",
+            "case_number": "JF180333",
+            "date": "2022-03-20T12:26:00.000",
+            "block": "044XX S GREENWOOD AVE",
+            "iucr": "051A",
+            "primary_type": "ASSAULT",
+            "description": "AGGRAVATED - HANDGUN",
+            "location_description": "APARTMENT",
+            "arrest": false,
+            "domestic": false,
+            "beat": "0222",
+            "district": "002",
+            "ward": "4",
+            "community_area": "39",
+            "fbi_code": "04A",
+            "x_coordinate": "1184335",
+            "y_coordinate": "1875674",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.814020748",
+            "longitude": "-87.59935555",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.59935555,
+                    41.814020748
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12647642",
+            "case_number": "JF180325",
+            "date": "2022-03-20T12:29:00.000",
+            "block": "036XX W DIVISION ST",
+            "iucr": "2092",
+            "primary_type": "NARCOTICS",
+            "description": "SOLICIT NARCOTICS ON PUBLIC WAY",
+            "location_description": "SIDEWALK",
+            "arrest": true,
+            "domestic": false,
+            "beat": "1112",
+            "district": "011",
+            "ward": "27",
+            "community_area": "23",
+            "fbi_code": "18",
+            "x_coordinate": "1151683",
+            "y_coordinate": "1907749",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.902741192",
+            "longitude": "-87.718283745",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.718283745,
+                    41.902741192
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12647664",
+            "case_number": "JF180350",
+            "date": "2022-03-20T12:29:00.000",
+            "block": "007XX N PULASKI RD",
+            "iucr": "4650",
+            "primary_type": "OTHER OFFENSE",
+            "description": "SEX OFFENDER - FAIL TO REGISTER",
+            "location_description": "SIDEWALK",
+            "arrest": false,
+            "domestic": false,
+            "beat": "1112",
+            "district": "011",
+            "ward": "37",
+            "community_area": "23",
+            "fbi_code": "26",
+            "x_coordinate": "1149588",
+            "y_coordinate": "1904743",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.894533379",
+            "longitude": "-87.72605727",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.72605727,
+                    41.894533379
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12647714",
+            "case_number": "JF180345",
+            "date": "2022-03-20T12:30:00.000",
+            "block": "009XX S INDEPENDENCE BLVD",
+            "iucr": "0486",
+            "primary_type": "BATTERY",
+            "description": "DOMESTIC BATTERY SIMPLE",
+            "location_description": "APARTMENT",
+            "arrest": false,
+            "domestic": true,
+            "beat": "1133",
+            "district": "011",
+            "ward": "24",
+            "community_area": "29",
+            "fbi_code": "08B",
+            "x_coordinate": "1151402",
+            "y_coordinate": "1895647",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.869537556",
+            "longitude": "-87.719633824",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.719633824,
+                    41.869537556
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12649386",
+            "case_number": "JF180339",
+            "date": "2022-03-20T12:30:00.000",
+            "block": "061XX S KENWOOD AVE",
+            "iucr": "0486",
+            "primary_type": "BATTERY",
+            "description": "DOMESTIC BATTERY SIMPLE",
+            "location_description": "APARTMENT",
+            "arrest": false,
+            "domestic": true,
+            "beat": "0314",
+            "district": "003",
+            "ward": "20",
+            "community_area": "42",
+            "fbi_code": "08B",
+            "x_coordinate": "1186131",
+            "y_coordinate": "1864459",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.78320366",
+            "longitude": "-87.593121999",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.593121999,
+                    41.78320366
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        },
+        {
+            "id": "12647892",
+            "case_number": "JF180696",
+            "date": "2022-03-20T12:30:00.000",
+            "block": "009XX N LAKE SHORE DR",
+            "iucr": "0910",
+            "primary_type": "MOTOR VEHICLE THEFT",
+            "description": "AUTOMOBILE",
+            "location_description": "STREET",
+            "arrest": false,
+            "domestic": false,
+            "beat": "1833",
+            "district": "018",
+            "ward": "2",
+            "community_area": "8",
+            "fbi_code": "07",
+            "x_coordinate": "1178543",
+            "y_coordinate": "1907201",
+            "year": "2022",
+            "updated_on": "2022-03-27T16:52:17.000",
+            "latitude": "41.900666632",
+            "longitude": "-87.619639652",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    -87.619639652,
+                    41.900666632
+                ]
+            },
+            "location_address": "",
+            "location_city": "",
+            "location_state": "",
+            "location_zip": ""
+        }
+  ]
+
   const searchResults = [
     {
         "id": "12647725",
@@ -20756,24 +21100,26 @@ const SearchTable = () => {
 //     }
 //   });
 
-  let handleClick = () => {
-    searchResults.forEach(e => {
-        console.log('On March ' + e.date.slice(8, 10) + ' ' + e.date.slice(0, 4) + ' at ' + e.date.slice(11, 16) + ', there was a ' + e.primary_type + ' (' + e.description + ') on ' + e.block.slice(6))
-        })
-    }
+//   let handleClick = () => {
+//     searchResults.forEach(e => {
+//         console.log('On March ' + e.date.slice(8, 10) + ' ' + e.date.slice(0, 4) + ' at ' + e.date.slice(11, 16) + ', there was a ' + e.primary_type + ' (' + e.description + ') on ' + e.block.slice(6))
+//         })
+//     console.log(document.getElementById("btn"));
+//     }
+
 
   return (
     <div>
-      <h2>This is where the SearchTable would render if TypeScript wasn't so picky</h2>
+      <h2>This is where the SearchTable component</h2>
       <h4>{searchResults.length} crimes were returned from your search.</h4>
       {/* <div>
           <canvas id="myChart" width="800" height="600"></canvas>
       </div> */}
       <div className="table">
-        <button id="btn" onClick={handleClick}>Get Crimes</button>
-        {/* {searchResults.forEach((e) => (
-            console.log(e.primary_type)
-        ))} */}
+        <button id="btn">Get Crimes</button>
+        {shortenedSearchResults.map((e, index) => (
+            <h5>{'On March ' + e.date.slice(8, 10) + ', ' + e.date.slice(0, 4) + ' at ' + e.date.slice(11, 16) + ', there was a ' + e.primary_type + ' (' + e.description + ') on ' + e.block.slice(6)}</h5>
+        ))}
       </div>
     </div>
   )
