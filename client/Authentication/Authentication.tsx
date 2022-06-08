7d6816c70554f8bd3de6303814ac6c3271f021aa
Teresa Gobble <teresagobble@gmail.com>
2022-06-08 17:00:37 -0500

    Initial Refactoring for Context, TODO comment removed from Auth

--- client/Authentication/Authentication.tsx
+++ client/Authentication/Authentication.tsx
@@ -5,7 +5,7 @@ import {
   onAuthStateChanged,
   signOut,
 } from "firebase/auth";
-// need to ask charles for firebase API key
+
 import { auth } from "./firebase-config";
 import { Box, Button, Typography, Modal } from '@mui/material';
 
@@ -52,7 +52,6 @@ const Authentication = () => {
         loginEmail,
         loginPassword
       );
-      // console.log('user being logged in: ', user);
       let existingAccountEmail: any = user.user.email;
       setUserEmail(existingAccountEmail);
       let welcomeMessage: string = "Welcome back " + existingAccountEmail;
@@ -67,7 +66,6 @@ const Authentication = () => {
   const logout = async () => {
     try {
       signOut(auth);
-      // console.log('user being logged out: ', user);
       window.alert("Successfully logged out");
       setUserEmail("");
       // un-hide registration/login modal
--- client/CrimesContext.js
+++ client/CrimesContext.js
@@ -1,62 +1,21 @@
 import React, { createContext, useState } from "react";
 
-const crimesMockData = [
-    {
-        "id": "1987808",
-        "case_number": "HH186280",
-        "date": "2002-02-14T11:57:00.000",
-        "block": "065XX S KOSTNER AV",
-        "iucr": "1010",
-        "primary_type": "ARSON",
-        "description": "BY EXPLOSIVE",
-        "location_description": "STREET",
-        "arrest": false,
-        "domestic": false,
-        "beat": "0833",
-        "district": "008",
-        "fbi_code": "09",
-        "x_coordinate": "1148168",
-        "y_coordinate": "1860816",
-        "year": "2002",
-        "updated_on": "2018-02-28T15:56:25.000",
-        "latitude": "41.774019167",
-        "longitude": "-87.732401195",
-        "location": {
-            "latitude": "41.774019167",
-            "longitude": "-87.732401195",
-            "human_address": "{\"address\": \"\", \"city\": \"\", \"state\": \"\", \"zip\": \"\"}"
-        }
-    },
-]
-
 const CrimesContext = createContext(); //setting CrimesContext as the context
-// WE NEED TO USE MY SERVER IF I'M EVER GOING TO SET CONTEXT
 
-const CrimesContextProvider = (props) => { //this acts as the wrapper for the components who must use the same state (essentially as a customized global scope, since all the children and children's children should be able to access this correctly without drilling)
+const CrimesContextProvider = (props) => { // this acts as the wrapper for the components who must use the same state (essentially as a customized global scope, since all the children and children's children should be able to access this correctly without drilling)
 
   const [crimes, setCrimes] = useState([]);
-  const [mapCenter, setMapCenter] = useState([41.8757, -87.6243]);
+  const [mapCenter, setMapCenter] = useState([41.8757, -87.6243]); // default is Downtown Chicago
   const [zoomRate, setZoomRate] = useState(11);
 
-  // app.get("/searched", (request, response) => {
-    //   const { location, crime, crimeSubcategory, date } = request.query;
-    //   getSearchedCrime({ location, crime, crimeSubcategory, date })
-    //     .then((crimeResults) => {
-      //       response.send(crimeResults);
-      //     })
-      //     .catch((error) => {
-        //       response.status(500).send(error);
-        //     })
-        // });
-
-        const CrimesProviderValue = {
-          crimes: crimes,
-          setCrimes: setCrimes,
-          mapCenter: mapCenter,
-          setMapCenter: setMapCenter,
-          zoomRate: zoomRate,
-          setZoomRate: setZoomRate
-        }
+  const CrimesProviderValue = {
+    crimes,
+    setCrimes,
+    mapCenter,
+    setMapCenter,
+    zoomRate,
+    setZoomRate
+  }
 
   return (
     <CrimesContext.Provider value={ CrimesProviderValue }>
